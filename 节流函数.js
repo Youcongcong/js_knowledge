2cf47b483d050d8afd5c2cdc6eff4deecde421b5
youcongcong <834153504@qq.com>
2018-05-03 23:04:06 +0800

    [UPDATE:JS]

--- 节流函数.js
+++ 节流函数.js
@@ -3,19 +3,19 @@
  * 节流函数不管事件触发多频繁，都会在规定的时间内执行处理函数
  */
 
- //实现方式：1、时间戳 2、定时器
+//实现方式：1、时间戳 2、定时器
 
- var throttle = function(func,delay){
-     var prev = Date.now();
-     return function(){
-         var context = this;
-         var args = arguments;
-         var now = Date.now();
-         if(now-prev >= delay){
+var throttle = function (func, delay) {
+    var prev = Date.now();
+    return function () {
+        var context = this;
+        var args = arguments;
+        var now = Date.now();
+        if (now - prev >= delay) {
             console.log(delay)
-             
-            func.call(context,args);
-             prev = Date.now()
-         }
-     }
- }
+
+            func.call(context, args);
+            prev = Date.now()
+        }
+    }
+}
